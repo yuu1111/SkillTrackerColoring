0ecfd2ecfa64e51f1fe730b83eca3685ba25fc30
yuu111 <yuu210yuu@gmail.com>
2025-01-21 05:03:11 +0900

    update 1.0.3

--- TextChange.cs
+++ TextChange.cs
@@ -45,12 +45,12 @@ namespace SkillTrackerColoring
                     learningText = new[] { "学習", "採取", "読書", "鑑定", "解剖学", "暗記", "錬金", "料理", "戦略" };
                     willText = new[] { "意志", "乗馬", "旅歩き", "釣り", "瞑想", "信仰" };
                     magicText = new[] { "魔力", "魔力制御", "魔力の限界", "詠唱", "魔道具" };
-                    charismaText = new[] { "魅力", "共存", "演奏", "交渉術", "投資" };
+                    charismaText = new[] { "魅力", "共生", "演奏", "交渉術", "投資" };
                     break;
 
                 case "EN":
                     strengthText = new[] { "Strength", "Weightlifting", "Mining", "Digging", "Carpentry", "Blacksmith", "Two Handed", "Martial Art", "Long Sword", "Axe", "Scythe" };
-                    enduranceText = new[] { "Endurance", "Swimming", "Taming", "Farming", "Regeneration", "Sculpture", "Heavy Armor", "Shield", "Staff", "Polearm", "Mace" };
+                    enduranceText = new[] { "Endurance", "Swimming", "Lumberjacking","Taming", "Farming", "Regeneration", "Sculpture", "Heavy Armor", "Shield", "Staff", "Polearm", "Mace" };
                     dexterityText = new[] { "Dexterity", "Lockpicking", "Pickpocket", "Disarm Trap", "Jewelry", "Weaving", "Crafting", "Throwing", "Light Armor", "Dual Wield", "Bow", "Short Sword" };
                     perceptionText = new[] { "Perception", "Stealth", "Spot Hidden", "Tactics", "Marksman", "Eye of Mind", "Evasion", "Greater Evasion", "Gun", "Crossbow" };
                     learningText = new[] { "Learning", "Gathering", "Literacy", "Appraising", "Anatomy", "Memorization", "Alchemy", "Cooking", "Strategy" };
@@ -63,7 +63,7 @@ namespace SkillTrackerColoring
                     strengthText = new[] { "力量", "举重", "挖掘", "挖洞", "木工", "锻造", "双手武器", "格斗", "长剑", "斧", "镰" };
                     enduranceText = new[] { "体质", "游泳", "伐木", "驯兽", "农业", "自愈", "雕刻", "重装备", "盾", "杖", "长枪", "钝器" };
                     dexterityText = new[] { "灵巧", "开锁", "偷窃", "解除陷阱", "宝石加工", "裁缝", "制造", "投掷", "轻装备", "二刀流", "弓", "短剑" };
-                    perceptionText = new[] { "感知", "隐蔽", "探索", "战术", "射击", "心眼", "闪避", "看穿", "弩", "弩机" };
+                    perceptionText = new[] { "感知", "隐蔽", "探索", "战术", "射击", "心眼", "闪避", "看穿", "枪械", "弩" };
                     learningText = new[] { "学习", "采集", "读书", "鉴定", "解剖学", "背诵", "炼金", "烹饪", "战略" };
                     willText = new[] { "意志", "骑乘", "旅行", "钓鱼", "冥想", "信仰" };
                     magicText = new[] { "魔力", "魔力控制", "魔力极限", "咏唱", "魔道具" };
